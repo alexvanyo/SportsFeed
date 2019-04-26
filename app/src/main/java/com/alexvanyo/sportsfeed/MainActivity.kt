738a5fea9b3acde9dcfba5d160f89d43f676c127
Alex Vanyo <vanyoalex@gmail.com>
2019-04-25 19:21:56 -0500

    Added basic RecyclerView

--- app/src/main/java/com/alexvanyo/sportsfeed/MainActivity.kt
+++ app/src/main/java/com/alexvanyo/sportsfeed/MainActivity.kt
@@ -2,11 +2,59 @@ package com.alexvanyo.sportsfeed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.text_view.view.*
+import android.view.LayoutInflater
+
+class TextViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val textView: TextView = itemView.text_view
+
+    fun bindData(text: String) {
+        textView.text = text
+    }
+}
+
+val DIFF_CALLBACK = object : DiffUtil.ItemCallback<String>() {
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class TextAdapter : ListAdapter<String, TextViewHolder>(DIFF_CALLBACK) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.text_view, parent, false)
+        return TextViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TextViewHolder, position: Int) {
+        holder.bindData(getItem(position))
+    }
+}
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textAdapter = TextAdapter()
+
+        textAdapter.submitList((1..100).map { i -> "Test $i" }.toList())
+
+        this.recycler_view.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = textAdapter
+        }
     }
 }
