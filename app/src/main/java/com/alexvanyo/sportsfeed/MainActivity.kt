33281ca83dbeded0b6df12c969caa0a1fd9f0ec9
Alex Vanyo <vanyoalex@gmail.com>
2019-04-26 19:42:41 -0500

    Created data binding objects

--- app/src/main/java/com/alexvanyo/sportsfeed/MainActivity.kt
+++ app/src/main/java/com/alexvanyo/sportsfeed/MainActivity.kt
@@ -2,26 +2,17 @@ package com.alexvanyo.sportsfeed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.text_view.view.*
 import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.alexvanyo.sportsfeed.databinding.GameItemBinding
 
-class TextViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val textView: TextView = itemView.text_view
-
-    fun bindData(text: String) {
-        textView.text = text
-    }
-}
-
-class TextAdapter : ListAdapter<String, TextViewHolder>(DIFF_CALLBACK) {
+class TextAdapter : ListAdapter<String, TextAdapter.DataBoundViewHolder<GameItemBinding>>(DIFF_CALLBACK) {
     companion object {
         val DIFF_CALLBACK = object : DiffUtil.ItemCallback<String>() {
             override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
@@ -34,14 +25,17 @@ class TextAdapter : ListAdapter<String, TextViewHolder>(DIFF_CALLBACK) {
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.text_view, parent, false)
-        return TextViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBoundViewHolder<GameItemBinding> {
+        return DataBoundViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.game_item, parent, false))
     }
 
-    override fun onBindViewHolder(holder: TextViewHolder, position: Int) {
-        holder.bindData(getItem(position))
+    override fun onBindViewHolder(holder: DataBoundViewHolder<GameItemBinding>, position: Int) {
+        holder.binding.text = getItem(position)
+        holder.binding.executePendingBindings()
     }
+
+    class DataBoundViewHolder<out T : ViewDataBinding> constructor(val binding: T) :
+        RecyclerView.ViewHolder(binding.root)
 }
 
 class MainActivity : AppCompatActivity() {
