138ed493e5d8277446ea8223da562d85f136cc72
Alex Vanyo <vanyoalex@gmail.com>
2019-05-05 01:39:36 -0500

    Setup default appbar with navigation

--- app/src/main/java/com/alexvanyo/sportsfeed/MainActivity.kt
+++ app/src/main/java/com/alexvanyo/sportsfeed/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.alexvanyo.sportsfeed
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.container)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
