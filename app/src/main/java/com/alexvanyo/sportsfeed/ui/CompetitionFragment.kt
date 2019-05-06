ab1d924fe03447ed04c22642758aa4976ffb7c47
Alex Vanyo <vanyoalex@gmail.com>
2019-05-06 01:26:45 -0500

    Improved FeedViewModel by separating out the polling observable

--- app/src/main/java/com/alexvanyo/sportsfeed/ui/CompetitionFragment.kt
+++ app/src/main/java/com/alexvanyo/sportsfeed/ui/CompetitionFragment.kt
@@ -1,10 +1,27 @@
 package com.alexvanyo.sportsfeed.ui
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.alexvanyo.sportsfeed.viewmodel.FeedViewModel
 import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
 /**
  * Fragment that displays the detail view for a specific competition
  */
 class CompetitionFragment : DaggerFragment() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var model: FeedViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        model = activity?.run {
+            ViewModelProviders.of(this, viewModelFactory).get(FeedViewModel::class.java)
+        } ?: throw Exception("Invalid Activity")
+    }
 }
--- app/src/main/java/com/alexvanyo/sportsfeed/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/alexvanyo/sportsfeed/viewmodel/FeedViewModel.kt
@@ -1,21 +1,59 @@
 package com.alexvanyo.sportsfeed.viewmodel
 
-import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.alexvanyo.sportsfeed.api.ESPNService
-import io.reactivex.BackpressureStrategy
+import com.alexvanyo.sportsfeed.api.ScoreboardData
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.Inject
 
 /**
  * Main view model for the feed.
  */
 class FeedViewModel @Inject constructor(private val espnService: ESPNService) : ViewModel() {
-    private val mlbDataObservable = Observable.interval(0, 10, TimeUnit.SECONDS)
-        .flatMap { espnService.getMLBGames() }
-        .subscribeOn(Schedulers.io()).toFlowable(BackpressureStrategy.LATEST)
 
-    val mlbData = LiveDataReactiveStreams.fromPublisher(mlbDataObservable)
+    private val shouldPoll = AtomicBoolean()
+
+    private val compositeDisposable = CompositeDisposable()
+
+    init {
+        shouldPoll.set(true)
+
+        compositeDisposable.add(Observable.interval(0, 10, TimeUnit.SECONDS)
+            .filter { shouldPoll.get() }
+            .flatMap { espnService.getMLBGames() }
+            .subscribeOn(Schedulers.io())
+            .subscribe { _mlbData.postValue(it) })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        compositeDisposable.clear()
+    }
+
+    // Override inactive/active callbacks to pause/resume network polling
+    private val _mlbData = object: MutableLiveData<ScoreboardData>() {
+        override fun onInactive() {
+            super.onInactive()
+
+            shouldPoll.set(false)
+        }
+
+        override fun onActive() {
+            super.onActive()
+
+            shouldPoll.set(true)
+        }
+
+    }
+
+    val mlbData: LiveData<ScoreboardData>
+        get() = _mlbData
+
 }
